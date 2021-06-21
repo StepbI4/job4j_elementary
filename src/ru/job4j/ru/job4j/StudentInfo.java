5935e99c3d9aa282d4bada4e10e3fcc12c947ac3
Nikita_Stepanov <stepanovn1993@gmail.com>
2021-06-21 22:17:57 +0300

    3.3. Разбор первой программы. [#159033]

---
+++ src/ru/job4j/ru/job4j/StudentInfo.java
@@ -0,0 +1,8 @@
+package ru.job4j.ru.job4j;
+
+public class StudentInfo {
+    public static void main(String[] args) {
+        System.out.println("Nikita Stepanov");
+        System.out.println("17.07.1993");
+    }
+}
